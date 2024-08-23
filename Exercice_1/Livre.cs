919bc3e139f80de17bd84661c9b0ec4961889cc9
Jucan <2249463@bdeb.qc.ca>
2024-08-23 10:30:54 -0400

    Exercice 1, sans catch exception

---
+++ Exercice_1/Livre.cs
@@ -0,0 +1,49 @@
+﻿using System.Text;
+
+namespace Exercice_1
+{
+    internal class Livre
+    {
+        private int _pages;
+        public string Titre { get; }
+        public string Auteur { get; }
+        public string Editeur { get; }
+        public int Pages
+        {
+            get
+            {
+                return _pages;
+            }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Nombre de pages doit etre plus grand que 0");
+                }
+                _pages = value;
+            }
+        }
+        private int Annee { get; }
+
+        public Livre(string titre, string auteur, string editeur, int nb_pages, int anee)
+        {
+
+            Titre = titre;
+            Auteur = auteur;
+            Editeur = editeur;
+            Pages = nb_pages;
+            Annee = anee;
+        }
+
+        public override string ToString()
+        {   
+            StringBuilder sb = new StringBuilder();
+            sb.Append($"Titre: {Titre}").Append(" ");
+            sb.Append($"Auteur: {Auteur}").Append(" ");
+            sb.Append($"Editeur: {Editeur}").Append(" ");
+            sb.Append($"Pages: {Pages}").Append(" ");
+            sb.Append($"Annee: {Annee}");
+            return sb.ToString();
+        }
+    }
+}
---
+++ Exercice_1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Exercice_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Livre[] livres = new Livre[3];
+
+            livres[0] = new Livre("Calculus 1", "John Stewart", "BIO", 492, 2023);
+            livres[1] = new Livre("How to Aim", "TenZ", "VALO", 102, 2022);
+            livres[2] = new Livre("Introduction to Programming", "Aymen Khlif", "BDEB", 2, 2019);
+            // livres[3] = new Livre("Calculus 1", "John Stewart", "BIO", 492, 2023);
+            // livres[4] = new Livre("Calculus 1", "John Stewart", "BIO", 492, 2023);
+
+            foreach(var livre in livres)
+            {
+                Console.WriteLine(livre.ToString());
+            }
+        }
+    }
+}
