473f6f0278dc53860548fb17ebbb11b43a6f1b90
Denis Jucan MAIN_PC <2249463@bdeb.qc.ca>
2024-08-29 21:50:19 -0400

    Exercice 2, partie insertions a la fin

--- M2_Exercise_Projet/ExerciseModule2/Program.cs
+++ M2_Exercise_Projet/ExerciseModule2/Program.cs
@@ -1,32 +1,66 @@
 ﻿using System.Diagnostics;
 using System.Text;
 
-class Program()
+namespace ExerciseModule2
 {
-    const uint NOMBRE_ITERATIONS = 100000; // Le nombre d'itérations
-    public static void Main(string[] args)
+    class Program()
     {
-        Stopwatch sw = new Stopwatch(); // Pour évaluer le temps des opérations
-                                        // On va concaténer les nombres de 0 à NOMBRE ITERATIONS-1 dans une
-                                        // chaîne de caractères
-        sw.Start();
-        string premiereChaine = "";
-        for (int i = 0; i < NOMBRE_ITERATIONS; i++)
+        const uint NOMBRE_ITERATIONS = 100000; // Le nombre d'itérations
+        public static void Main(string[] args)
         {
-            premiereChaine = premiereChaine + i + ' ';
+            TestInsertionsFin();
         }
-        sw.Stop();
-        Console.WriteLine($"La concatenation a pris {sw.ElapsedMilliseconds} millisecondes");
-        // On fait la même chose mais cette fois avec un StringBuilder
-        sw.Restart();
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < NOMBRE_ITERATIONS; i++)
+        public static void TestInsertionsFin()
         {
-            sb.Append(i);
-            sb.Append(' ');
+            const uint nb_insertions = 125000;
+
+            Stopwatch sw = new Stopwatch();
+            List<int> list = new List<int>();
+            LinkedList<int> linkedlist = new LinkedList<int>();
+
+            sw.Start();
+            // Insertions par la fin
+            for (int i = 0; i < nb_insertions; i++)
+            {
+                list.Add(i);
+            }
+            sw.Stop();
+            Console.Write($"Nombre de millisecondes pour remplir List<> par la fin: {sw.ElapsedMilliseconds}");
+            Console.WriteLine("\n");
+            sw.Restart();
+            for (int i = 0; i < nb_insertions; i++)
+            {
+                linkedlist.AddLast(i);
+            }
+            Console.WriteLine($"Nombre de millisecondes pour remplir LinkedList<> par la fin : {sw.ElapsedMilliseconds}");
+
+
+
         }
-        string deuxiemeChaine = sb.ToString();
-        sw.Stop();
-        Console.WriteLine($"L'utilisation de StringBuilder a pris {sw.ElapsedMilliseconds} millisecondes");
+        public static void TestStringBuilder()
+        {
+            Stopwatch sw = new Stopwatch(); // Pour évaluer le temps des opérations
+                                            // On va concaténer les nombres de 0 à NOMBRE ITERATIONS-1 dans une
+                                            // chaîne de caractères
+            sw.Start();
+            string premiereChaine = "";
+            for (int i = 0; i < NOMBRE_ITERATIONS; i++)
+            {
+                premiereChaine = premiereChaine + i + ' ';
+            }
+            sw.Stop();
+            Console.WriteLine($"La concatenation a pris {sw.ElapsedMilliseconds} millisecondes");
+            // On fait la même chose mais cette fois avec un StringBuilder
+            sw.Restart();
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < NOMBRE_ITERATIONS; i++)
+            {
+                sb.Append(i);
+                sb.Append(' ');
+            }
+            string deuxiemeChaine = sb.ToString();
+            sw.Stop();
+            Console.WriteLine($"L'utilisation de StringBuilder a pris {sw.ElapsedMilliseconds} millisecondes");
+        }
     }
 }
